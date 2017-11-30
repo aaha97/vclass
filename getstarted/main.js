1519462cfdd74773b065d29843f66d5bfa802a9d
Arif Awate <aaha97@gmail.com>
2017-12-01 00:22:00 +0530

    checkpoint 2 achieved student details
    added the function to get student details
    removed alert
    added a delay because of the unknown 500 internal error on instant function calls

--- getstarted/main.js
+++ getstarted/main.js
@@ -1,4 +1,33 @@
-alert("hi")
+var delayInMilliseconds = 5000; //1 second
+
+setTimeout(function() {
+  getTimeTable();
+	getStudentDetails();
+}, delayInMilliseconds);
+
+function getStudentDetails() {
+    var http = new XMLHttpRequest();
+    var url = "studentsRecord/SearchRegnoStudent";
+    var data = "verifyMenu=true";
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function() { //Call a function when the state changes.
+        if (http.readyState == 4 && http.status == 200) {
+            var res = http.responseText;
+            var hiddenElement = document.createElement('a');
+
+            hiddenElement.href = 'data:attachment/text,' + encodeURI(res);
+            hiddenElement.target = '_blank';
+            hiddenElement.download = 'myFile.html';
+            hiddenElement.click();
+        }
+    }
+    http.send(data);
+	return 0;
+}
 
 function getTimeTable() {
     var http = new XMLHttpRequest();
@@ -16,7 +45,7 @@ function getTimeTable() {
 
             hiddenElement.href = 'data:attachment/text,' + encodeURI(res);
             hiddenElement.target = '_blank';
-            hiddenElement.download = 'myFile.txt';
+            hiddenElement.download = 'myFile.html';
             hiddenElement.click();
         }
     }
@@ -24,4 +53,4 @@ function getTimeTable() {
 	return 0;
 }
 
-getTimeTable();
+
