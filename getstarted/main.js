a68c8f168cc2824a8a507ccf54a051e6bb9a2e08
Arif Awate <aaha97@gmail.com>
2017-11-30 23:25:25 +0530

    checkpoint 1 achieved
    timetable fetched as html and script beautified
    keeping that alert for testing errors

--- getstarted/main.js
+++ getstarted/main.js
@@ -1,15 +1,27 @@
 alert("hi")
-var http = new XMLHttpRequest();
-var url = "processViewTimeTable";
-var data = "semesterSubId=" + 'VL2017185';
-http.open("POST", url, true);
 
-//Send the proper header information along with the request
-http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+function getTimeTable() {
+    var http = new XMLHttpRequest();
+    var url = "processViewTimeTable";
+    var data = "semesterSubId=" + 'VL2017185';
+    http.open("POST", url, true);
 
-http.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function() { //Call a function when the state changes.
+        if (http.readyState == 4 && http.status == 200) {
+            var res = http.responseText;
+            var hiddenElement = document.createElement('a');
+
+            hiddenElement.href = 'data:attachment/text,' + encodeURI(res);
+            hiddenElement.target = '_blank';
+            hiddenElement.download = 'myFile.txt';
+            hiddenElement.click();
+        }
     }
+    http.send(data);
+	return 0;
 }
-http.send(data);
+
+getTimeTable();
