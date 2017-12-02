a720cfba6c06eb4167753609811b4d09cecd9848
Arif Awate <aaha97@gmail.com>
2017-12-03 03:19:42 +0530

    combining 2 checkpoints
    fetched personal info
    fetched timetable
    re organized fetched data and ready to feed into database

--- getstarted/main.js
+++ getstarted/main.js
@@ -1,8 +1,10 @@
 var delayInMilliseconds = 5000; //1 second
-
+var name = "";
+var regno = "";
+var argtable = []
 setTimeout(function() {
-  getTimeTable();
 	getStudentDetails();
+	getTimeTable();
 }, delayInMilliseconds);
 
 function getStudentDetails() {
@@ -17,12 +19,16 @@ function getStudentDetails() {
     http.onreadystatechange = function() { //Call a function when the state changes.
         if (http.readyState == 4 && http.status == 200) {
             var res = http.responseText;
-            var hiddenElement = document.createElement('a');
+			var domparser = new DOMParser;
+			var doc = domparser.parseFromString(res,"text/html");
+			collectPersonalInfo(doc);
+            /*var hiddenElement = document.createElement('a');
 
             hiddenElement.href = 'data:attachment/text,' + encodeURI(res);
             hiddenElement.target = '_blank';
             hiddenElement.download = 'myFile.html';
             hiddenElement.click();
+			*/
         }
     }
     http.send(data);
@@ -41,16 +47,46 @@ function getTimeTable() {
     http.onreadystatechange = function() { //Call a function when the state changes.
         if (http.readyState == 4 && http.status == 200) {
             var res = http.responseText;
-            var hiddenElement = document.createElement('a');
-
-            hiddenElement.href = 'data:attachment/text,' + encodeURI(res);
-            hiddenElement.target = '_blank';
-            hiddenElement.download = 'myFile.html';
-            hiddenElement.click();
+			var domparser = new DOMParser;
+			var doc = domparser.parseFromString(res,"text/html");
+			collectTimeTableInfo(doc);
         }
     }
     http.send(data);
 	return 0;
 }
 
+function collectPersonalInfo(res){
+	var profile = res.getElementsByClassName("table")[0];
+	name = profile["rows"][2]["cells"][1].textContent;
+	regno = profile["rows"][16]["cells"][1].textContent;
+}
 
+function collectTimeTableInfo(res){
+	var timetable = res.getElementsByClassName("table")[1];
+	var k=0;
+	for(var i=4;i<=16;i+=2){
+		for(var j=2;j<=15;j++){
+			if(j==8){
+			}else{
+				if(timetable["rows"][i]["cells"][j].textContent.length>=6){
+					argtable.push(timetable["rows"][i]["cells"][j].textContent);
+				}else{
+					argtable.push("empty");
+				}				
+			}
+		}
+	}
+	for(var i=5;i<=17;i+=2){
+		for(var j=1;j<=14;j++){
+			if(j==7){
+			}else{
+				if(timetable["rows"][i]["cells"][j].textContent.length>=6){
+					argtable.push(timetable["rows"][i]["cells"][j].textContent);
+				}else{
+					argtable.push("empty");
+				}				
+			}
+		}
+	}
+}
